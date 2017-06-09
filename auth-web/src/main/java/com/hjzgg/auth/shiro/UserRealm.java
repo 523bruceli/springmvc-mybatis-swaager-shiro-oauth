d497fdf7729433ceaf33baf7516dae978b2b9b26
hujunzheng <hujunzheng@beiquan.org>
2017-06-09 16:06:19 +0800

    提交一下

--- auth-web/src/main/java/com/hjzgg/auth/shiro/UserRealm.java
+++ auth-web/src/main/java/com/hjzgg/auth/shiro/UserRealm.java
@@ -7,6 +7,8 @@ import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Resource;
 
@@ -75,5 +77,4 @@ public class UserRealm extends AuthorizingRealm {
         clearAllCachedAuthenticationInfo();
         clearAllCachedAuthorizationInfo();
     }
-
 }
