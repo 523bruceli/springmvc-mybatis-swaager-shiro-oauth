5b7b6e97b74bfab74e935e670112c697750f6ef6
hujunzheng <hujunzheng@beiquan.org>
2017-07-15 19:27:17 +0800

    提交一下

--- auth-web/src/main/java/com/hjzgg/auth/shiro/UserRealm.java
+++ auth-web/src/main/java/com/hjzgg/auth/shiro/UserRealm.java
@@ -7,8 +7,6 @@ import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.annotation.Resource;
 import java.util.Arrays;
